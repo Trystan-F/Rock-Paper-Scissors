e06c0aece4bc72a775b6c37138adcfdf4fab4492
Trystan-F <trystanfakes@gmail.com>
2024-09-04 15:52:47 +0100

    improvements and results 4/9

--- scripts.js
+++ scripts.js
@@ -1,47 +1,51 @@
 // Rock Paper Scissors
 
 getComputerChoice = () => {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = ["rock", "paper", "scissors"];
   let pcChoice = Math.floor(Math.random() * choices.length);
   return choices[pcChoice];
 };
 
 getPlayerChoice = () => {
-  let userChoice = prompt('rock, paper or scissors?');
-  return userChoice
-  
+  let userChoice = prompt("rock, paper or scissors?");
+  return userChoice;
 };
-
-const computerChoice = getComputerChoice()
-const playerChoice = getPlayerChoice().toLowerCase();
 
 playerScore = 0;
 computerScore = 0;
 
-playRound = (computerChoice, playerChoice) =>  {
+playRound = (computerChoice, playerChoice) => {
   if (computerChoice === playerChoice) {
-    console.log("It's a draw! You both threw", computerChoice)
+    console.log("It's a draw! You both threw", computerChoice);
   } else if (
-    (computerChoice === 'rock' && playerChoice === 'paper') ||
-    (computerChoice === 'paper' && playerChoice === 'scissors') ||
-    (computerChoice === 'scissors' && playerChoice === 'rock')
+    (computerChoice === "rock" && playerChoice === "paper") ||
+    (computerChoice === "paper" && playerChoice === "scissors") ||
+    (computerChoice === "scissors" && playerChoice === "rock")
   ) {
-    console.log(`You won! You threw ${playerChoice} and the computer threw ${computerChoice}`)
-    playerScore ++
+    console.log(
+      `You won! You threw ${playerChoice} and the computer threw ${computerChoice}`
+    );
+    playerScore++;
   } else {
-    console.log(`You lost! You threw ${playerChoice} and the computer threw ${computerChoice}`)
-    computerScore ++
+    console.log(
+      `You lost! You threw ${playerChoice} and the computer threw ${computerChoice}`
+    );
+    computerScore++;
   }
-  console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`)
-}
-
-const playGame = () => {
- for (let i = 0; i < 5; i++) {
-  const computerChoice = getComputerChoice();
-  const playerChoice = getPlayerChoice().toLowerCase()
-  playRound(computerChoice, playerChoice);
-  }
+  console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
 };
 
+const playGame = () => {
+  for (let i = 0; playerScore < 3 && computerScore < 3; i++) {
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice().toLowerCase();
+    playRound(computerChoice, playerChoice);
+  }
+  if (playerScore === 3) {
+    alert(`You won the game! ${playerScore} - ${computerScore}`);
+  } else {
+    alert(`You lost the game! ${playerScore} - ${computerScore}`);
+  }
+};
 
 playGame();
